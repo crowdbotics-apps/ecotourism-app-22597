aabb6550cd3c99896077f9fa8c25866114c68fd5
Crowdbotics <team@crowdbotics.com>
2020-11-13 19:42:51 +0000

    Update models

--- backend/ecotourism_app_22597/wsgi.py
+++ backend/ecotourism_app_22597/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecotourism_app_22597.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ecotourism_app_22597.settings")
 
 application = get_wsgi_application()
